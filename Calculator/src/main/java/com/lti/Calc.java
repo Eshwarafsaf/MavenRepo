8822d71e12d41005d9a9cace1a9739ca28ca9d9d
Eshwarafsaf <eshwarkrishnan99@gmail.com>
2020-12-21 15:09:02 +0530

    Products

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,6 +8,9 @@ public class Calc {
 	public static int sub(int num1, int num2) {
 		return num1 - num2;
 	}
+	public static int prod(int num1, int num2) {
+		return num1 * num2;
+	}
 	public static void main(String[] args) {
 		System.out.println(add(30, 43));
 		System.out.println(sub(30, 43));
