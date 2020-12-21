74f3569e4dd4ebb9fcb1507bf7c5b11315027dd5
Eshwarafsaf <eshwarkrishnan99@gmail.com>
2020-12-21 14:59:19 +0530

    Subs

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,8 +5,11 @@ public class Calc {
 	public static int add(int num1, int num2) {
 		return num1 + num2;
 	}
-
+	public static int sub(int num1, int num2) {
+		return num1 - num2;
+	}
 	public static void main(String[] args) {
 		System.out.println(add(30, 43));
+		System.out.println(sub(30, 43));
 	}
 }
